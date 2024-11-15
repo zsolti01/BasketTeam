89778d67d51bd8c7566af5f74b1e3b51a83fcdbd
vecseizsolt <vecseizs@kkszki.hu>
2024-11-15 13:20:23 +0100

    A feladat elkészült.

---
+++ Connect.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasketTeam
+{
+    public class Connect
+    {
+        public MySqlConnection Connection;
+        public string Host;
+        public string Database;
+        public string User;
+        public string Password;
+        public string ConnectionString;
+
+        public Connect()
+        {
+            Host = "localhost";
+            Database = "team";
+            User = "root";
+            Password = "";
+
+            ConnectionString = "SERVER=" + Host + ";DATABASE=" + Database + ";UID=" + User + ";PASSWORD=" + Password + ";SslMode=None";
+            
+            Connection = new MySqlConnection(ConnectionString);
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using ZstdSharp.Unsafe;
+
+namespace BasketTeam
+{
+    internal class Program
+    {
+
+        public static Connect conn = new Connect();
+
+        public static void GetAllData()
+        {
+            conn.Connection.Open();
+
+            string sql = "SELECT * FROM `player`";
+
+            MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
+
+            MySqlDataReader dr = cmd.ExecuteReader();
+
+            dr.Read();
+
+            do
+            {
+                var player = new
+                {
+                    Id = dr.GetInt32(4),
+                    Name = dr.GetString(0),
+                    Height = dr.GetInt32(1),
+                    Weight = dr.GetInt32(2),
+                    CreatedTime = dr.GetDateTime(3)
+                };
+
+                Console.WriteLine($"Játékos adatok: {player.Name},{player.Height}");
+
+            } while (dr.Read());
+            
+            dr.Close();
+
+            conn.Connection.Close();
+        }
+
+        static void Main(string[] args)
+        {
+            GetAllData();
+        }
+    }
+}
